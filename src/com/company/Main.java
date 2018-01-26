21a1d66b1bbb8398b491448ad9a4cd138a2e7347
JustinYates <Justin.yates24601@gmail.com>
2018-01-26 15:11:32 -0500

    newest controlF

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,59 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //While Loops
+
+        boolean whileCondition = false;
+        int count = 0;
+
+        while (whileCondition) {
+            System.out.println("Our while condition is true.  Adding 1 to our count");
+            count = count + 1;
+            if (count == 3) {
+                System.out.println("Our count is at 3");
+                whileCondition = false;
+            }
+        }
+
+        //Do While
+
+        boolean doWhileCondition = false;
+        count = 0;
+
+        do {
+            System.out.println("Our do while condition is true.  Adding 1 to our count");
+            count = count + 1;
+            if (count == 3) {
+                System.out.println("Our count is at 3");
+                doWhileCondition = false;
+            }
+        } while (doWhileCondition);
+
+        //For Loops
+
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+        //Foreach
+
+        ArrayList<String> fruit = new ArrayList<String>();
+        fruit.add("Apple");
+        fruit.add("Banana");
+        fruit.add("Orange");
+        fruit.add("Strawberry");
+        fruit.add("Grape");
+
+        for (String food : fruit) {
+            if (food.contains("b") || food.contains("B")) {
+                System.out.println(food);
+            }
+        }
+
     }
 }
